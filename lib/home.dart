17f94db1a25ad007a77945d9c1dd1b4e31467077
aya <ayakamal4545@gmail.com>
2024-07-24 15:59:51 +0300

    delet

--- lib/home.dart
+++ lib/home.dart
@@ -7,9 +7,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Islami"),
-      ),
+      appBar: AppBar(),
     );
+
+
   }
 }
